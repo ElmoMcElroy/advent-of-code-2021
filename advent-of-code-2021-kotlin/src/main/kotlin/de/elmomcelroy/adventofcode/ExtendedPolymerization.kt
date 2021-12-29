f3c984bf645db133d43921eaf3db0fb28fb85e6b
Steffen <uwe@dieter.ulf>
2021-12-29 21:59:22 +0100

    task 14.2

--- advent-of-code-2021-kotlin/src/main/kotlin/de/elmomcelroy/adventofcode/ExtendedPolymerization.kt
+++ advent-of-code-2021-kotlin/src/main/kotlin/de/elmomcelroy/adventofcode/ExtendedPolymerization.kt
@@ -1,23 +1,71 @@
 package de.elmomcelroy.adventofcode
 
-import java.lang.StringBuilder
-
 class ExtendedPolymerization {
 
     private val rules: MutableMap<String, String> = mutableMapOf()
+    private val knownCharCounts: MutableMap<Pair<String, Int>, Map<Char, Long>> = mutableMapOf()
 
     /**
      * rekursiv. baum links n schritte. dann rechts bis n-ter schritt erreicht
-     * merken welche buchstaben bei n-ten schritt
+     * merken welche buchstaben für polymer Paar bei n-ten schritt
+     *
+     *  Template:     NNCB
+     *  After step 1: NCNBCHB
+     *  After step 2: NBCCNBBBCBHCB
+     *  After step 3: NBBBCNCCNBBNBNBBCHBHHBCHB
+     *  After step 4: NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB
      */
+    fun countCharacters(polymerRules: List<String>, steps: Int): MutableMap<Char, Long> {
+        rules.clear()
+        knownCharCounts.clear()
+        val charsCountAll = mutableMapOf<Char, Long>()
+        val polymer = parseInput(polymerRules)
+        charsCountAll[polymer.first()] = charsCountAll.getOrDefault(polymer.first(), 0) + 1
 
+        for (pos in 2 until polymer.length + 1) {
+            val charsCount = applyRulesRecursive(polymer.substring(pos - 2, pos), steps, 0)
+            charsCount.keys.forEach { keyChar -> charsCountAll.merge(keyChar, charsCount[keyChar]!!) { prev, one -> prev + one } }
+            println("charsCount: $charsCount")
+            println("charsCountAll: $charsCountAll")
+            println()
+        }
+
+        return charsCountAll
+    }
+
+    private fun applyRulesRecursive(polymerPair: String, maxStep: Int, step: Int): Map<Char, Long> {
+
+        if (step == maxStep) {
+            return mutableMapOf(Pair(polymerPair.last(), 1))
+        }
+        val newChar = rules[polymerPair]!!
+        val charsCountMerged: Map<Char, Long>
+        if (!knownCharCounts.containsKey(Pair(polymerPair, step))) {
+            val charsCountLeft = applyRulesRecursive(polymerPair.first() + newChar, maxStep, step + 1)
+            val charsCountRight = applyRulesRecursive(newChar + polymerPair.last(), maxStep, step + 1)
+            charsCountMerged = (charsCountLeft.keys + charsCountRight.keys).associateWith{ charsCountLeft.getOrDefault(it, 0).plus(charsCountRight.getOrDefault(it, 0)) }
+
+            knownCharCounts[Pair(polymerPair, step)] = charsCountMerged
+        } else {
+            charsCountMerged = knownCharCounts[Pair(polymerPair, step)]!!
+        }
+//        println("KnownCharCount[${Pair(polymerPair, step)}]: ${knownCharCounts[Pair(polymerPair, step)]}")
+//        println()
+        return charsCountMerged
+
+    }
+
+
+    fun calculateScore(charsCount: MutableMap<Char, Long>): Long {
+        return charsCount.values.maxOrNull()!! - charsCount.values.minOrNull()!!
+    }
 
     fun calculateScore(polymer: String): Long {
         val charsCount = mutableMapOf<Char, Long>()
         polymer.forEach{
             charsCount[it] = charsCount.getOrDefault(it, 0) + 1
         }
-        return charsCount.values.maxOrNull()!! - charsCount.values.minOrNull()!!
+        return calculateScore(charsCount)
     }
 
     fun extendPolymer(polymerRules: List<String>, steps: Int): String {
--- advent-of-code-2021-kotlin/src/test/kotlin/de/elmomcelroy/adventofcode/Task14Test.kt
+++ advent-of-code-2021-kotlin/src/test/kotlin/de/elmomcelroy/adventofcode/Task14Test.kt
@@ -14,17 +14,18 @@ class Task14Test {
 
         val polymerAfter4Steps: String = subjectUnderTest.extendPolymer(polymerRules, 4)
         assertEquals("NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB", polymerAfter4Steps, "Polymer after 4 steps")
+        val scoreAfter4 = subjectUnderTest.calculateScore(polymerAfter4Steps)
+        assertEquals(18, scoreAfter4, "Score after 4 steps")
+
         val polymerAfter5Steps: String = subjectUnderTest.extendPolymer(polymerRules, 5)
         assertEquals(97, polymerAfter5Steps.length, "Polymer length after 5 steps")
+        val scoreAfter5 = subjectUnderTest.calculateScore(polymerAfter5Steps)
+        assertEquals(33, scoreAfter5, "Score after 5 steps")
+
         val polymerAfter10Steps: String = subjectUnderTest.extendPolymer(polymerRules, 10)
         assertEquals(3073, polymerAfter10Steps.length, "Polymer length after 10 steps")
-        val score = subjectUnderTest.calculateScore(polymerAfter10Steps)
-        assertEquals(1588, score, "Polymer score after 10 steps")
-
-        val polymerAfter40Steps: String = subjectUnderTest.extendPolymer(polymerRules, 40)
-//        assertEquals(3073, polymerAfter40Steps.length, "Polymer length after 40 steps")
-//        val scoreAfter40 = subjectUnderTest.calculateScore(polymerAfter40Steps)
-//        assertEquals(2188189693529, scoreAfter40, "Polymer score after 10 steps")
+        val scoreAfter10 = subjectUnderTest.calculateScore(polymerAfter10Steps)
+        assertEquals(1588, scoreAfter10, "Polymer score after 10 steps")
     }
 
     @Test
@@ -35,6 +36,40 @@ class Task14Test {
         assertEquals(19457, polymerAfter10Steps.length, "Polymer length after 10 steps")
         val score = subjectUnderTest.calculateScore(polymerAfter10Steps)
         assertEquals(2360, score, "Polymer score after 10 steps")
+    }
+
+    @Test
+    fun testExtendedPolymerizationRecursive() {
+        val polymerRules = Utils.readFileAsString("/task14/test.txt")
+
+        val charsAfter4Steps = subjectUnderTest.countCharacters(polymerRules, 4)
+        val scoreAfter4 = subjectUnderTest.calculateScore(charsAfter4Steps)
+        assertEquals(18, scoreAfter4, "Score after 4 steps")
+
+        val charsAfter5Steps = subjectUnderTest.countCharacters(polymerRules, 5)
+        val scoreAfter5 = subjectUnderTest.calculateScore(charsAfter5Steps)
+        assertEquals(33, scoreAfter5, "Score after 5 steps")
+
+        val charsAfter10Steps = subjectUnderTest.countCharacters(polymerRules, 10)
+        val scoreAfter10 = subjectUnderTest.calculateScore(charsAfter10Steps)
+        assertEquals(1588, scoreAfter10, "Score after 10 steps")
+
+        val charsAfter40Steps = subjectUnderTest.countCharacters(polymerRules, 40)
+        val scoreAfter40 = subjectUnderTest.calculateScore(charsAfter40Steps)
+        assertEquals(2188189693529, scoreAfter40, "Score after 10 steps")
+    }
+
+    @Test
+    fun testExtendedPolymerizationRecursiveWithFile() {
+        val polymerRules = Utils.readFileAsString("/task14/input.txt")
+
+        val charsAfter10Steps = subjectUnderTest.countCharacters(polymerRules, 10)
+        val score = subjectUnderTest.calculateScore(charsAfter10Steps)
+        assertEquals(2360, score, "Polymer score after 10 steps")
+
+        val charsAfter40Steps = subjectUnderTest.countCharacters(polymerRules, 40)
+        val scoreAfter40 = subjectUnderTest.calculateScore(charsAfter40Steps)
+        assertEquals(2967977072188, scoreAfter40, "Score after 10 steps")
     }
 
 }
